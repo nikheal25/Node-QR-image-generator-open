ab84a59ba07a73fa84b440c1838ee22448a9baa2
Nick <nikheal25@gmail.com>
2020-12-02 17:46:09 +1100

    final

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var qr = require("qr-image");
 
-const codes = ["123", "234"];
+const parse = require("csv-parse");
+const fs = require("fs");
 
-codes.forEach((element) => {
-  var qr_svg = qr.image(element, { type: "png" });
-  qr_svg.pipe(require("fs").createWriteStream(`./uploads/${element}.png`));
-});
+const data = [];
+fs.createReadStream("./new.csv")
+  .pipe(parse({ delimiter: "," }))
+  .on("data", (r) => {
+    // console.log(r[0]);
+    data.push(`${r[0]}`);
+  })
+  .on("end", () => {
+    // console.log(data);
+    console.log(typeof data);
+
+    data.forEach((element) => {
+      var qr_svg = qr.image(element, { type: "png" });
+      qr_svg.pipe(require("fs").createWriteStream(`./uploads/${element}.png`));
+    });
+  });
+
+// const codes = ["123", "234"];
+
+// codes.forEach((element) => {
+//   var qr_svg = qr.image(element, { type: "png" });
+//   qr_svg.pipe(require("fs").createWriteStream(`./uploads/${element}.png`));
+// });
 
 // var qr_svg = qr.image("I love QR!", { type: "png" });
 // qr_svg.pipe(require("fs").createWriteStream("i_love_qr.png"));
